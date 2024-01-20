7563b4901996ce18fb07cd81bdc0674da7c2775a
BadGamesInc <ibadgames5000@gmail.com>
2024-01-20 12:46:42 -0700

    :3

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -4,7 +4,8 @@
 
 package frc.robot;
 
-import frc.robot.Constants.OperatorConstants;
+import frc.robot.commands.LaunchNote;
+// import frc.robot.Constants.OperatorConstants;
 import frc.robot.commands.SwerveJoystickCmd;
 import frc.robot.subsystems.ShooterSubsystem;
 import frc.robot.subsystems.SwerveDriveSubsystem;
@@ -13,18 +14,18 @@ import edu.wpi.first.wpilibj2.command.Command;
 
 public class RobotContainer {
   private final SwerveDriveSubsystem swerve_drive_subsystem = new SwerveDriveSubsystem();
-  private final ShooterSubsystem shooterSubsystem;
+  private final ShooterSubsystem shooterSubsystem = new ShooterSubsystem();
 
   public RobotContainer() {
     // swerve_drive_subsystem.r
 
-    swerve_drive_subsystem.setDefaultCommand(new SwerveJoystickCmd(
-      swerve_drive_subsystem, 
-      () -> -driver_controller.getLeftY(), // Y-Axis 
-      () -> driver_controller.getLeftX(),  // X-Axis
-      () -> driver_controller.getRightX(),  // Rot-Axis
-      () -> !driver_controller.getHID().getAButtonPressed()  // Any button to set field orientation
-    ));
+    // swerve_drive_subsystem.setDefaultCommand(new SwerveJoystickCmd(
+    //   swerve_drive_subsystem, 
+    //   () -> -driver_controller.getLeftY(), // Y-Axis 
+    //   () -> driver_controller.getLeftX(),  // X-Axis
+    //   () -> driver_controller.getRightX(),  // Rot-Axis
+    //   () -> !driver_controller.getHID().getAButtonPressed()  // Any button to set field orientation
+    // ));
     // Configure the trigger bindings
     configureBindings();
   }
@@ -37,6 +38,9 @@ public class RobotContainer {
       () -> Constants.IO.controller.getRightX(),  // Rot-Axis
       () -> !Constants.IO.controller.getAButtonPressed()  // Any button to set field orientation
     ));
+    Constants.IO.commandController.a().whileTrue(shooterSubsystem.intakeCommand());
+    Constants.IO.commandController.b().whileTrue(new LaunchNote(shooterSubsystem));
+
 
   }
 
