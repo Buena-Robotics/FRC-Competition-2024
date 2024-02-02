194bec5a17bcbb9104fc8b5bc446b162f0bcd7c5
Daniel Raygoza <68447327+Primal-Illusion@users.noreply.github.com>
2024-02-02 15:50:09 -0700

    Adding sum subsys type shiiii

---
+++ src/main/java/frc/robot/subsystems/NoteArmSubsystem.java
@@ -0,0 +1,82 @@
+package frc.robot.subsystems;
+
+import com.revrobotics.ColorSensorV3;
+import com.revrobotics.ColorSensorV3.ColorSensorMeasurementRate;
+import com.revrobotics.ColorSensorV3.ColorSensorResolution;
+import com.revrobotics.ColorSensorV3.GainFactor;
+import com.revrobotics.ColorSensorV3.LEDCurrent;
+import com.revrobotics.ColorSensorV3.LEDPulseFrequency;
+
+import edu.wpi.first.util.sendable.SendableBuilder;
+import edu.wpi.first.wpilibj.DoubleSolenoid;
+import edu.wpi.first.wpilibj.I2C;
+import edu.wpi.first.wpilibj.PneumaticsModuleType;
+import edu.wpi.first.wpilibj.Solenoid;
+import edu.wpi.first.wpilibj.DoubleSolenoid.Value;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class NoteArmSubsystem extends SubsystemBase {
+    private static final PneumaticsModuleType PNEUMATICS_MODULE_TYPE = PneumaticsModuleType.REVPH;
+    private static final int CLAW_SOLENOID_FORWARD_CHANNEL    = -1;
+    private static final int CLAW_SOLENOID_REVERSE_CHANNEL    = -1;
+    private static final int ARM_UP_SOLENOID_FORWARD_CHANNEL  = -1;
+    private static final int ARM_UP_SOLENOID_REVERSE_CHANNEL  = -1;
+    private static final int ARM_OUT_SOLENOID_FORWARD_CHANNEL = -1;
+    private static final int ARM_OUT_SOLENOID_REVERSE_CHANNEL = -1;
+    private static final I2C.Port COLOR_SENSOR_PORT   = I2C.Port.kOnboard;
+
+    private final DoubleSolenoid claw_solenoid    = new DoubleSolenoid(PNEUMATICS_MODULE_TYPE, CLAW_SOLENOID_FORWARD_CHANNEL, CLAW_SOLENOID_REVERSE_CHANNEL);
+    private final DoubleSolenoid arm_up_solenoid  = new DoubleSolenoid(PNEUMATICS_MODULE_TYPE, ARM_UP_SOLENOID_FORWARD_CHANNEL, ARM_UP_SOLENOID_REVERSE_CHANNEL);
+    private final DoubleSolenoid arm_out_solenoid = new DoubleSolenoid(PNEUMATICS_MODULE_TYPE, ARM_OUT_SOLENOID_FORWARD_CHANNEL, ARM_OUT_SOLENOID_REVERSE_CHANNEL);
+    private final ColorSensorV3 color_sensor = new ColorSensorV3(COLOR_SENSOR_PORT);
+
+    public NoteArmSubsystem(){
+        // color_sensor.configureColorSensor(
+        //     ColorSensorResolution.kColorSensorRes13bit, 
+        //     ColorSensorMeasurementRate.kColorRate25ms, 
+        //     GainFactor.kGain1x);
+        // color_sensor.configureProximitySensorLED(
+        //     LEDPulseFrequency.kFreq100kHz, 
+        //     LEDCurrent.kPulse10mA, 
+        //     0);
+    }
+
+    public boolean isClawOpen(){ return claw_solenoid.get() == Value.kForward; }
+    public boolean isArmUp()   { return arm_up_solenoid.get() == Value.kForward; }
+    public boolean isArmOut()  { return arm_out_solenoid.get() == Value.kForward; }
+
+    public void openClaw()  { claw_solenoid.set(Value.kForward); }
+    public void moveArmUp() { arm_up_solenoid.set(Value.kForward); }
+    public void moveArmOut(){ arm_out_solenoid.set(Value.kForward); }
+
+    public void closeClaw()  { claw_solenoid.set(Value.kReverse); }
+    public void moveArmDown(){ arm_up_solenoid.set(Value.kReverse); }
+    public void moveArmIn()  { arm_out_solenoid.set(Value.kReverse); }
+
+
+    public double milimetersFromObject(){
+        final int PROXIMITY_SENSOR_MAX_VALUE = 2047;
+        final int MAX_DETECTING_DISTANCE_MM = 100; // 10 CM in specs
+        final double PROXIMITY_VALUE_PER_MM = (double)PROXIMITY_SENSOR_MAX_VALUE / MAX_DETECTING_DISTANCE_MM; 
+        int proximity_11bit = color_sensor.getProximity();
+
+        return proximity_11bit * PROXIMITY_VALUE_PER_MM;
+    }
+    public boolean detectingNoteColor(){
+        return false;
+    }
+
+    public boolean noteDetected(){
+        return milimetersFromObject() < 10 && detectingNoteColor();
+    }
+
+
+    @Override public void periodic() {
+        if(!isClawOpen() && !isArmUp() && !isArmOut() && noteDetected()) closeClaw();
+    }
+
+
+    @Override public void initSendable(SendableBuilder builder) {
+
+    }
+}
--- src/main/java/frc/robot/subsystems/ShooterSubsystem.java
+++ src/main/java/frc/robot/subsystems/ShooterSubsystem.java
@@ -2,30 +2,44 @@ package frc.robot.subsystems;
 
 import com.revrobotics.CANSparkMax;
 import com.revrobotics.RelativeEncoder;
+import com.revrobotics.CANSparkBase.FaultID;
 import com.revrobotics.CANSparkLowLevel.MotorType;
 
+import edu.wpi.first.util.sendable.SendableBuilder;
+import edu.wpi.first.wpilibj.DutyCycleEncoder;
+import edu.wpi.first.wpilibj.Encoder;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class ShooterSubsystem extends SubsystemBase {
-    
-    private static final int FEED_MOTOR_ID = 10, LAUNCH_MOTOR_ID = 9;
-    private static final int FEED_SMART_CURRENT_LIMIT = 80, LAUNCH_SMART_CURRENT_LIMIT = 80;
-    public static final double FEED_SPEED = 1, LAUNCH_SPEED = 1;
-    public static final double INTAKE_LAUNCH_SPEED = -1, INTAKE_FEED_SPEED = -.2;
+    private static final int FEED_MOTOR_ID = 10;
+    private static final int LAUNCH_MOTOR_ID = 9;
+    private static final int FEED_SMART_CURRENT_LIMIT = 80; 
+    private static final int LAUNCH_SMART_CURRENT_LIMIT = 80;
+    public static final double FEED_SPEED = 1;
+    public static final double LAUNCH_SPEED = 1;
+    public static final double INTAKE_LAUNCH_SPEED = -1; 
+    public static final double INTAKE_FEED_SPEED = -.2;
 
-    public CANSparkMax feed_motor, launch_motor;
-    public RelativeEncoder feed_motor_encoder, launch_motor_encoder;
+    private static final int BORE_ENCODER_CHANNEL = -1;
+
+    public final CANSparkMax feed_motor = new CANSparkMax(FEED_MOTOR_ID, MotorType.kBrushless);;
+    public final CANSparkMax launch_motor = new CANSparkMax(LAUNCH_MOTOR_ID, MotorType.kBrushless);
+    public final RelativeEncoder feed_motor_encoder;
+    public final RelativeEncoder launch_motor_encoder;
+
+    public final DutyCycleEncoder bore_encoder = new DutyCycleEncoder(BORE_ENCODER_CHANNEL);
 
     public ShooterSubsystem() {
-        feed_motor = new CANSparkMax(FEED_MOTOR_ID, MotorType.kBrushless);
-        launch_motor = new CANSparkMax(LAUNCH_MOTOR_ID, MotorType.kBrushless);
-
         feed_motor_encoder = feed_motor.getEncoder();
         launch_motor_encoder = launch_motor.getEncoder();
 
         feed_motor.setSmartCurrentLimit(FEED_SMART_CURRENT_LIMIT);
         launch_motor.setSmartCurrentLimit(LAUNCH_SMART_CURRENT_LIMIT);
+
+        bore_encoder.setPositionOffset(-1);
+        bore_encoder.setDistancePerRotation(-1);
+        bore_encoder.setDistancePerRotation(-1);
     }
 
     public double getFeedMotorVelocity(){ return feed_motor_encoder.getVelocity(); }
@@ -43,5 +57,11 @@ public class ShooterSubsystem extends SubsystemBase {
         launch_motor.set(0);
     }
 
+    public double getAngleDegrees(){ return bore_encoder.getAbsolutePosition(); }
+
     @Override public void periodic(){}
+
+    @Override public void initSendable(SendableBuilder builder) {
+
+    }
 }
