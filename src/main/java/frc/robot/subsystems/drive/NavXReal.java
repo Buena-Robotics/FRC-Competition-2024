1e63d8ea751e08033b927c1ea9667e71ad205ef3
Daniel Raygoza <raygoza.d@hotmail.com>
2024-03-01 11:30:53 -0700

    navx pose_estimation + more merge conflicts prolly; lol so silly

--- src/main/java/frc/robot/subsystems/drive/NavXReal.java
+++ src/main/java/frc/robot/subsystems/drive/NavXReal.java
@@ -4,11 +4,27 @@ import edu.wpi.first.math.util.Units;
 import edu.wpi.first.wpilibj.SerialPort;
 
 public class NavXReal extends NavX {
+    private Pose2d estimated_pose = new Pose2d();
+    private double last_timestamp = Timer.getFPGATimestamp();
+    private double delta_time = 0;
+
     public NavXReal(SerialPort.Port serial_port_id){ 
         super(serial_port_id); 
         this.resetDisplacement();
     }
     
+    private void updateOdometer(){
+        estimated_pose.transformBy(
+            new Transform2d(
+                inputs.velocity_x * delta_time, 
+                inputs.velocity_y * delta_time, 
+                Rotation2d.fromDegrees(inputs.angle_rate * delta_time)));
+
+        double timestamp = Timer.getFPGATimestamp();
+        delta_time = timestamp - last_timestamp; 
+        last_timestamp = timestamp;
+    }
+
     @Override public void updateInputs() {
         inputs.calibrating = super.isCalibrating();
         inputs.moving = super.isMoving();
@@ -19,16 +35,19 @@ public class NavXReal extends NavX {
         inputs.temperature_celcius = super.getTempC();
 
         inputs.angle = super.getRotation2d();
-        inputs.roll_radians = Units.degreesToRadians(super.getRoll());
-        inputs.pitch_radians = Units.degreesToRadians(super.getPitch());
-        inputs.yaw_radians = Units.degreesToRadians(super.getYaw());
+        inputs.angle_rate = super.getRate(); //TODO: Add to this Sim
+        inputs.roll_radians = removeNoise(Units.degreesToRadians(super.getRoll()));
+        inputs.pitch_radians = removeNoise(Units.degreesToRadians(super.getPitch()));
+        inputs.yaw_radians = removeNoise(Units.degreesToRadians(super.getYaw()));
 
-        inputs.velocity_x = super.getVelocityX();
-        inputs.velocity_y = super.getVelocityY();
-        inputs.velocity_z = super.getVelocityZ();
+        inputs.velocity_x = removeNoise(super.getVelocityX());
+        inputs.velocity_y = removeNoise(super.getVelocityY());
+        inputs.velocity_z = removeNoise(super.getVelocityZ());
 
-        inputs.displacement_x = super.getDisplacementX();
-        inputs.displacement_y = super.getDisplacementY();
-        inputs.displacement_z = super.getDisplacementZ();
+        inputs.displacement_x = removeNoise(super.getDisplacementX());
+        inputs.displacement_y = removeNoise(super.getDisplacementY());
+        inputs.displacement_z = removeNoise(super.getDisplacementZ());
+
+        inputs.estimated_pose = estimated_pose;
     }
 }
