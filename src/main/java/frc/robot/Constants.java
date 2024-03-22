cd9a3aa3223ac6c98b9c0990dceb77deea86cff5
BadGamesInc <ibadgames5000@gmail.com>
2024-03-22 00:34:20 -0700

    maybe some optimizations

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -4,8 +4,11 @@
 
 package frc.robot;
 
+import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.math.geometry.Rotation3d;
+import edu.wpi.first.math.geometry.Transform2d;
 import edu.wpi.first.math.geometry.Transform3d;
+import edu.wpi.first.math.geometry.Translation2d;
 import edu.wpi.first.math.util.Units;
 import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj2.command.button.Trigger;
@@ -16,13 +19,13 @@ import frc.robot.subsystems.drive.SwerveDrive;
 import frc.robot.subsystems.notearm.NoteArm;
 import frc.robot.subsystems.notearm.NoteArmReal;
 import frc.robot.subsystems.notearm.NoteArmSim;
-import frc.robot.subsystems.servo.ServoSub;
 import frc.robot.subsystems.shooter.Shooter;
 import frc.robot.subsystems.shooter.ShooterReal;
 import frc.robot.subsystems.shooter.ShooterSim;
 import frc.robot.subsystems.vision.Vision;
 import frc.robot.subsystems.vision.Vision.CameraData;
 import frc.robot.subsystems.vision.VisionCamera.PhotonPipeline;
+import frc.robot.utils.LocalADStarAK;
 import frc.robot.vendor.CXboxController;
 
 public final class Constants {
@@ -42,7 +45,6 @@ public final class Constants {
         public static final Shooter shooter = Robot.isReal() ? new ShooterReal() : new ShooterSim();
         public static final NoteArm note_arm = Robot.isReal() ? new NoteArmReal() : new NoteArmSim();
         public static final SwerveDrive swerve_drive = new SwerveDrive();
-        public static final ServoSub servo = new ServoSub();
         public static final Vision vision = new Vision(
                 new CameraData("Microsoft_LifeCam_HD-3000 (1) (2)", new Transform3d(
                         Units.inchesToMeters(12.5),
@@ -56,5 +58,11 @@ public final class Constants {
                         new Rotation3d(0,Units.degreesToRadians(40), 0)), PhotonPipeline.NOTE_DETECTION)
                         );
                         //6inches back
+    }
+
+    public static class Empty {
+        public static final Rotation3d R3D_ZERO = new Rotation3d();
+        public static final Rotation2d R2D_ZERO = new Rotation2d();
+        public static final Translation2d TRANSL2D_ZERO = new Translation2d();
     }
 }
